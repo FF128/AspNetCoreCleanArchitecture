0076f6c101531012fd7134b23b0fc1b462b60858
128 Development Team <developerpremium@128techconsultinginc.com>
2019-07-09 14:31:17 +0800

    first commit

---
+++ src/Infrastructure/Data/ConnectionFactory.cs
@@ -0,0 +1,11 @@
+﻿using Infrastructure.Interface;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infrastructure.Data
+{
+    public class ConnectionFactory : IConnectionFactory
+    {
+    }
+}
---
+++ src/Infrastructure/Interface/IConnectionFactory.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infrastructure.Interface
+{
+    public interface IConnectionFactory
+    {
+    }
+}
